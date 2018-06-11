63a6ebc609862eb331441e7d341c636cb0f9b1f5
Paul Tyshevskyi <ptyshevs@gmail.com>
2018-06-11 16:43:26 +0300

    Multi-line name/comment cutting works...almost as it should

--- src/asm.c
+++ src/asm.c
@@ -12,6 +12,18 @@
 
 #include "asm.h"
 
+void	pass_lines(t_list **lines, int cnt)
+{
+	int	i;
+
+	i = 0;
+	while (i < cnt)
+	{
+		*lines = (*lines)->next;
+		i++;
+	}
+}
+
 /*
 ** Parse name. Handle multi-line names
 ** @param asms
@@ -23,10 +35,73 @@ void	parse_name(t_asm *asms, t_list *lines)
 	asms->name = ft_strdup(lines->content);
 }
 
-void	parse_comment(t_asm *asms, t_list *lines)
+/*
+** Cut a comment, start to which is pointed by <comment>. Traverse multiple
+** lines if necessary.
+*/
+
+char	*cut_comment(char *comment, t_list *lines)
 {
-	ft_printf("parsing comment: %s\n", lines->content);
-	asms->comment = ft_strdup(lines->content);
+	char	*collect;
+	char	*tmp;
+
+	if (ft_strchr(comment, '"'))
+		return (ft_strtok(comment, "\""));
+	collect = ft_strdup(comment);
+	lines = lines->next;
+	while (lines && !ft_strchr(lines->content, '"'))
+	{
+		tmp = collect;
+		collect = ft_sjoin(3, collect, lines->content, "\n");
+		ft_strdel(&tmp);
+		lines = lines->next;
+	}
+	if (!lines) // Non enclosing double-quote
+		return (NULL);
+	// find closing double-quote
+	// calculate distance to it
+	size_t len = ft_strchr(lines->content, '"') - (char *)lines->content;
+	char *another = ft_strtrunc(lines->content, len, FALSE);
+	tmp = collect;
+	collect = ft_sjoin(3, collect, "\n", another);
+	ft_strdel(&tmp);
+	return (collect);
+}
+
+char	*ft_strchr_ignore_comments(char *str, char c)
+{
+
+}
+
+/*
+** Cut string in form "somestring" after a <what> field
+*/
+
+char	*cut_string(t_asm *asms, t_list **lines, char *what)
+{
+	char	*line;
+	int		cnt_lines;
+	char	*tmp;
+
+	line = (*lines)->content;
+	while (*line && ft_isspace(*line))
+		line++;
+	if (!ft_startswith(line, what))
+		return (NULL);
+	else
+		line += ft_slen(what);
+	while (*line && ft_isspace(*line))
+		line++;
+	if (*line != '"')
+		return (NULL);
+	line++; // skip opening double-quote
+	if (ft_strchr(line, '"') && (tmp = ft_strtok(line, "\"")))
+		return (tmp);
+	tmp = cut_comment(line, *lines);
+	cnt_lines = ft_strcnt(tmp, '\n');
+	asms->line_cnt += cnt_lines;
+	pass_lines(lines, cnt_lines);
+	return (tmp);
 }
 
 /*
@@ -52,9 +127,9 @@ void	parse_name_comment(t_asm *asms)
 			continue ;
 		}
 		else if (ft_strstr(lines->content, NAME_CMD_STRING))
-			parse_name(asms, lines);
+			asms->name = cut_string(asms, &lines, NAME_CMD_STRING);
 		else if (ft_strstr(lines->content, COMMENT_CMD_STRING))
-			parse_comment(asms, lines);
+			asms->comment = cut_string(asms, &lines, COMMENT_CMD_STRING);
 		else
 			syntax_error(asms, lines);
 		if (asms->name && asms->comment)
@@ -85,5 +160,6 @@ int main(int ac, char **av)
 	read_file(&asms);
 //	lexical_analysis(asms.lines);
 	parse_name_comment(&asms);
+	ft_printf("name: %s\ncomment: %s\n", asms.name, asms.comment);
 	wrap_up(&asms);
 }
