603056af104bcf658c62e44fde0b1c50f49f3d44
Oleksandra Pokusynska <opokusyn@student.unit.ua>
2018-06-14 14:49:08 +0300

    reads comment

--- src/corewar.c
+++ src/corewar.c
@@ -59,7 +59,7 @@ unsigned int	check_magic(unsigned char *magic)
 	i = 0;
 	while (i < 4)
 		nbr = (nbr << 8) + magic[i++];
-	ft_printf("magic is %0X\n", nbr);
+	// ft_printf("magic is %0X\n", nbr);
 
 	if (nbr != COREWAR_EXEC_MAGIC)
 		ft_panic("Magic is bad", 2, 1);
@@ -77,7 +77,7 @@ unsigned char	*find_magic(unsigned char *file)
 	j = 0;
 	while (i < 4)
 		first_four[j++] = file[i++];
-	ft_printf("%d|%d|%d|%d\n", first_four[0], first_four[1], first_four[2], first_four[3]);
+	// ft_printf("%d|%d|%d|%d\n", first_four[0], first_four[1], first_four[2], first_four[3]);
 	return (first_four);
 }
 
@@ -106,7 +106,8 @@ unsigned char	*find_size(unsigned char *file)
 	j = 0;
 	while (i < PROG_NAME_LENGTH + 12)
 		size[j++] = file[i++];
-	ft_printf("%d|%d|%d|%d\n", size[0], size[1], size[2], size[3]);
+	// ft_printf("%d|%d|%d|%d\n", size[0], size[1], size[2], size[3]);
+	// ft_printf("%d\n", i);
 	return (size);
 }
 
@@ -135,9 +136,9 @@ char			*find_name(unsigned char *file)
 	while (i < PROG_NAME_LENGTH + 1)
 		name[j++] = file[i++];
 	j = 0;
-	while (j < PROG_NAME_LENGTH + 1)
-		ft_printf("%d|", name[j++]);
-	ft_printf("\n");
+	// while (j < PROG_NAME_LENGTH + 1)
+	// 	ft_printf("%d|", name[j++]);
+	// ft_printf("\n");
 	return (name);
 }
 
@@ -161,6 +162,44 @@ char			*check_name(char *name)
 	return (ret);
 }
 
+char			*find_comment(unsigned char *file)
+{
+	char			*comment;
+	int				i;
+	int				j;
+
+	comment = ft_memalloc(sizeof(char) * (COMMENT_LENGTH + 1));
+	i = 140;
+	j = 0;
+	while (i < 140 + COMMENT_LENGTH + 1)
+		comment[j++] = file[i++];
+	j = 0;
+	// while (j < COMMENT_LENGTH + 1)
+	// 	ft_printf("%d|", comment[j++]);
+	// ft_printf("\n");
+	return (comment);
+}
+
+char			*check_comment(char *comment)
+{
+	unsigned int	nbr;
+	char			*ret;
+	int				i;
+	int				j;
+
+	nbr = 0;
+	i = 0;
+	j = 0;
+	ret = ft_memalloc(sizeof(char) * (COMMENT_LENGTH + 1));
+	while (comment[i] != 0)
+	{
+		nbr = 0;
+		nbr = (nbr << 8) + comment[i++];
+		ret[j++] = nbr;
+	}
+	return (ret);
+}
+
 void			complete_file(char *file_name)
 {
 	unsigned char	*file;
@@ -173,6 +212,9 @@ void			complete_file(char *file_name)
 	bot->name = check_name(find_name(file));
 	ft_printf("name is %s\n", bot->name);
 	bot->size = check_size(find_size(file));
+	ft_printf("size is %i\n", bot->size);
+	bot->comment = check_comment(find_comment(file));
+	ft_printf("comment is %s\n", bot->comment);
 }
 
 int				main(int ac, char **av)
