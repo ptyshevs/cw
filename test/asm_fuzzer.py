d22baa5799c2b06e3bd5ffd96c3c6fcbe28a14f4
Paul Tyshevskyi <ptyshevs@gmail.com>
2018-06-16 19:53:53 +0300

    Fuzzer is almost finished

--- test/asm_fuzzer.py
+++ test/asm_fuzzer.py
@@ -25,6 +25,7 @@ smaller.
 WARNING: only the output is compared, not the files generated (comes in the future releases)
 """
 from fuzzer import Fuzzer
+import os
 import sys
 import random
 import string
@@ -32,104 +33,12 @@ import string
 [*] Store/update path to my asm and to original asm in pickle
 [*] Ask for a directory with *.s files
 [*] Implement all 3 modes of file manipulation
-[ ] Record good *.s files to the database
+[ ] Open all *.s files and store somewhere as originals
 """
-mild_charset = '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~'
-specs = ";\"'#$%^&*()[],-.=>?\|/\\}{"
-spaces = [9, 10, 11, 12, 13, 32]
-def gen_rand_string(mode, length):
-    """
-    Generate random string. Available modes:
-    1) "digits" - Digits only, with optional sign at the beginning
-    2) "label" - Label-like (lowercase and with `:` in random places)
-    3) "bizarre" - Non-sense (complete Unicode charset)
-    4) "spec" - Start or end with a special character
-    5) "spaces" - string containing spaces only
-    :param mode: mode of operation
-    :param length: length of the desired string
-    :return: The random string
-    """
-    if mode == 'digits':
-        s = ''
-        if random.randint(0, 2) == 0:  # one third of all digit strings would contain a sign
-            s = '+' if random.randint(0, 1) == 1 else '-'
-        return s + ''.join([random.choice(string.digits) for _ in range(length if len(s) == 0 else length - 1)])
-    elif mode == 'label':
-        s = ''.join([random.choice(string.ascii_lowercase) for _ in range(length - 1)])
-        if random.randint(0, 9) == 0: # convert to uppercase
-            i = random.randrange(0, length - 1)
-            s = s[:i] + s[i].upper() + s[i+1:]
-        if random.randint(0, 4) < 4:
-            return s + ':'
-        else:
-            i = random.randrange(0, length)
-            return s[:i] + ':' + s[i:]
-    elif mode == 'bizarre':
-        return ''.join([chr(random.randint(0, 30000)) for _ in range(length)])
-    elif mode == 'spec':
-        i = random.randint(0, 1)  # beginning or end
-        spec = random.choice(specs)
-        s = ''.join([random.choice(string.ascii_lowercase) for _ in range(length - 1)])
-        return spec + s if i else s + spec
-    elif mode == 'spaces':
-        return ''.join([chr(random.choice(spaces)) for _ in range(length)])
-    else:
-        raise ValueError("mode is not recognized:", mode)
-
-def remove(file, n, k, l):
-    """Remove k+l segment"""
-    file[n] = file[n][:k] + file[n][k + l:]
-
-
-def insert(file, n, k, l, mode='spec'):
-    """
-        insert l random characters in k-th position
-
-        There are two :
-        1) Add complete nonsense from almost whole UTF-8 charset
-        2) Add english chars
-    :param file: file's content
-    :param n: random line selected
-    :param k: random position on the line
-    :param l: length of changed segment
-    :param mode: Mode of operation
-    :return: Nothing
-    """
-    file[n] = file[n][:k + 1] + gen_rand_string(mode, l) + file[n][k+1:]
-
-def replace(file, n, k, l, mode='spec'):
-    """
-    Replace l characters, starting from k with random values
-    :param file: file's content
-    :param n: random line selected
-    :param k: random position on the line
-    :param l: length of changed segment
-    :param mode: Mode of operation
-    """
-    file[n] = file[n][:k] + gen_rand_string(mode, l) + file[n][k+l:]
-
-def open_file(filename):
-    with open(filename, 'r') as f:
-        return f.readlines()
-
-
-def select_random_location(file: list):
-    """Find a new random position (n, k) in a file, where n - line, and k - position in a line"""
-    n = random.randrange(0, len(file))
-    k = random.randrange(0, len(file[n]))
-    return n, k
 
 
 if __name__ == '__main__':
-    file = open_file(sys.argv[1])
-    operations = [("insert", insert), ("remove", remove), ("replace", replace)]
-    l = 3
-    for i in range(10):
-        opname, op = random.choice(operations)
-        print("op selected:", opname)
-        n, k = select_random_location(file)
-        print("orig:", file[n])
-        op(file, n, k, l)
-        print("mod:", file[n])
-    # fz = Fuzzer()
-    # fz.db.wrap_up()
+    fz = Fuzzer()
+    fz.open_files()
+    fz.fuzz_once()
+    fz.db.wrap_up()
--- test/fuzzer.py
+++ test/fuzzer.py
@@ -1,11 +1,160 @@
 from db import Db
+import os
 import sys
-
+import subprocess
+import random
+import string
 
 class Fuzzer:
+    specs = ";\"'#$%^&*()[],-.=>?\|/\\}{"
+    spaces = [9, 10, 11, 12, 13, 32]
+
     def __init__(self):
         self.args = self.parse_args()
         self.db = self.init_db()
+        self.operations = [("insert", Fuzzer.insert), ("remove", Fuzzer.remove), ("replace", Fuzzer.replace)]
+        self.files = dict()
+        self.out_dir = 'traces'
+        self.l = 1
+        self.m = 5
+        if not os.path.exists(self.out_dir):
+            os.makedirs(self.out_dir)
+
+
+    def open_files(self):
+        """Find all files with *.s extension in src_dir"""
+        for file in os.listdir(self.db.get('src_dir')):
+            if file.endswith(".s") and file not in self.files.keys():
+                with open(self.db.get('src_dir') + "/" + file) as f:
+                    self.files[file] = f.readlines()
+
+    def asm_run(self, mod_filename):
+        """Compare original asm output with your version"""
+        orig_command = "{} {}".format(self.db.get('true_asm'), mod_filename)
+        my_command = "{} {}".format(self.db.get('my_asm'), mod_filename)
+        try:  # Normal execution
+            orig_output = subprocess.check_output(orig_command, shell=True, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:  # Error
+            orig_output = str(e.output, encoding='utf-8')
+        try:  # Same here
+            mod_output = subprocess.check_output(my_command, shell=True, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            mod_output = str(e.output, encoding='utf-8')
+        print("Original output:", orig_output)
+        print("Mod output:", mod_output)
+
+    def fuzz_once(self):
+        filename, file = random.choice(list(self.files.items()))
+        file = self.mod_file(file, self.m)
+        mod_filename = self.save_file(file, filename)
+        self.asm_run(mod_filename)
+
+    def mod_file(self, file, m):
+        mod_file = [line for line in file]
+        for i in range(m):
+            opname, op = random.choice(self.operations)
+            n, k = self.select_random_location(mod_file)
+            op(mod_file, n, k, self.l)
+        return mod_file
+
+    @staticmethod
+    def gen_rand_string(mode, length):
+        """
+        Generate random string. Available modes:
+        1) "digits" - Digits only, with optional sign at the beginning
+        2) "label" - Label-like (lowercase and with `:` in random places)
+        3) "bizarre" - Non-sense (complete Unicode charset)
+        4) "spec" - Start or end with a special character
+        5) "spaces" - string containing spaces only
+        :param mode: mode of operation
+        :param length: length of the desired string
+        :return: The random string
+        """
+        if mode == 'digits':
+            s = ''
+            if random.randint(0, 2) == 0:  # one third of all digit strings would contain a sign
+                s = '+' if random.randint(0, 1) == 1 else '-'
+            return s + ''.join([random.choice(string.digits) for _ in range(length if len(s) == 0 else length - 1)])
+        elif mode == 'label':
+            s = ''.join([random.choice(string.ascii_lowercase) for _ in range(length - 1)])
+            if random.randint(0, 9) == 0:  # convert to uppercase
+                i = random.randrange(0, length - 1)
+                s = s[:i] + s[i].upper() + s[i + 1:]
+            if random.randint(0, 4) < 4:
+                return s + ':'
+            else:
+                i = random.randrange(0, length)
+                return s[:i] + ':' + s[i:]
+        elif mode == 'bizarre':
+            return ''.join([chr(random.randint(0, 30000)) for _ in range(length)])
+        elif mode == 'spec':
+            i = random.randint(0, 1)  # beginning or end
+            spec = random.choice(Fuzzer.specs)
+            s = ''.join([random.choice(string.ascii_lowercase) for _ in range(length - 1)])
+            return spec + s if i else s + spec
+        elif mode == 'spaces':
+            return ''.join([chr(random.choice(Fuzzer.spaces)) for _ in range(length)])
+        else:
+            raise ValueError("mode is not recognized:", mode)
+
+    @staticmethod
+    def remove(file, n, k, l):
+        """Remove k+l segment"""
+        file[n] = file[n][:k] + file[n][k + l:]
+
+    @staticmethod
+    def insert(file, n, k, l, mode='spec'):
+        """
+            insert l random characters in k-th position
+
+            There are two :
+            1) Add complete nonsense from almost whole UTF-8 charset
+            2) Add english chars
+        :param file: file's content
+        :param n: random line selected
+        :param k: random position on the line
+        :param l: length of changed segment
+        :param mode: Mode of operation
+        :return: Nothing
+        """
+        file[n] = file[n][:k + 1] + Fuzzer.gen_rand_string(mode, l) + file[n][k + 1:]
+
+    @staticmethod
+    def replace(file, n, k, l, mode='spec'):
+        """
+        Replace l characters, starting from k with random values
+        :param file: file's content
+        :param n: random line selected
+        :param k: random position on the line
+        :param l: length of changed segment
+        :param mode: Mode of operation
+        """
+        file[n] = file[n][:k] + Fuzzer.gen_rand_string(mode, l) + file[n][k + l:]
+
+    @staticmethod
+    def open_file(filename):
+        with open(filename, 'r') as f:
+            return f.readlines()
+
+    @staticmethod
+    def select_random_location(file: list):
+        """Find a new random position (n, k) in a file, where n - line, and k - position in a line"""
+        n = random.randrange(0, len(file))
+        k = random.randrange(0, len(file[n]))
+        return n, k
+
+    def save_file(self, file, filename):
+        """
+        Generate filename
+        :param file: file content
+        :param filename of the file
+        :return: None
+        """
+        new_filename = os.path.join(self.out_dir, os.path.basename(filename)[:-2] + ''.join(
+            [random.choice(string.ascii_lowercase) for _ in range(20)]) + '.s')
+        with open(new_filename, 'w+') as f:
+            f.writelines(file)
+        return new_filename
 
     @staticmethod
     def parse_args():
