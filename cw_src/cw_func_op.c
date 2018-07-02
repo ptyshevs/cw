636a090b9cd95baf3b3323773c2dc19f61de5467
Paul Tyshevskyi <ptyshevs@gmail.com>
2018-07-02 10:31:57 +0300

    Add copy-pasted code of other function instructions

--- cw_src/cw_func_op.c
+++ cw_src/cw_func_op.c
@@ -25,28 +25,72 @@ void	i_add(t_map *map, t_proc *pr)
 		pr->reg[pr->args[2].value] = val;
 	else
 		log_more(map, "i_add tried to write to r%d\n", pr->args[2].value);
+	pr->carry = (t_uint)(val == 0);
 }
+
+/*
+** T_REG(0) - T_REG(1) -> T_REG(2)
+*/
 
 void	i_sub(t_map *map, t_proc *pr)
 {
-	(void)map;
-	(void)pr;
+	t_uint	val;
+
+	val = pr->args[0].value - pr->args[1].value;
+	if (pr->args[2].value < 16)
+		pr->reg[pr->args[2].value] = val;
+	else
+		log_more(map, "i_sub tried to write to r%d\n", pr->args[2].value);
+	pr->carry = (t_uint)(val == 0);
 }
+
+/*
+** arg(0) and arg(1) can be of any type
+** arg(0) & arg(1) -> T_REG(2)
+*/
 
 void	i_and(t_map *map, t_proc *pr)
 {
-	(void)map;
-	(void)pr;
+	t_uint	val;
+
+	val = pr->args[0].value & pr->args[1].value;
+	if (pr->args[2].value < 16)
+		pr->reg[pr->args[2].value] = val;
+	else
+		log_more(map, "i_and tried to write to r%d\n", pr->args[2].value);
+	pr->carry = (t_uint)(val == 0);
 }
+
+/*
+** arg(0) and arg(1) can be of any type
+** arg(0) | arg(1) -> T_REG(2)
+*/
 
 void	i_or(t_map *map, t_proc *pr)
 {
-	(void)map;
-	(void)pr;
+	t_uint	val;
+
+	val = pr->args[0].value | pr->args[1].value;
+	if (pr->args[2].value < 16)
+		pr->reg[pr->args[2].value] = val;
+	else
+		log_more(map, "i_or tried to write to r%d\n", pr->args[2].value);
+	pr->carry = (t_uint)(val == 0);
 }
+
+/*
+** arg(0) and arg(1) can be of any type
+** arg(0) ^ arg(1) -> T_REG(2)
+*/
 
 void	i_xor(t_map *map, t_proc *pr)
 {
-	(void)map;
-	(void)pr;
+	t_uint	val;
+
+	val = pr->args[0].value ^ pr->args[1].value;
+	if (pr->args[2].value < 16)
+		pr->reg[pr->args[2].value] = val;
+	else
+		log_more(map, "i_xor tried to write to r%d\n", pr->args[2].value);
+	pr->carry = (t_uint)(val == 0);
 }
